28919e0992e778359200ce13e903085e903157b1
SanjayMalviya20 <malviyasanjay8875438@gmail.com.com>
2025-03-31 22:15:05 +0530

    ui updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 "use-client"
 
-import { Facebook01Icon, Facebook02Icon, InstagramIcon, TwitterIcon } from "hugeicons-react"
+import {  Contact02Icon, ContactIcon, Facebook02Icon, InstagramIcon } from "hugeicons-react"
 import Link from "next/link"
 
 const Footer = () => {
@@ -11,13 +11,14 @@ const Footer = () => {
         <p className="text-center"> Copyright @SanjayMalviya | All rights reserved.</p>
         
       </div >
-      <div className="flex gap-4 px-3">
+      <div className="flex gap-4 px-3 items-center">
         <Link href="https://www.instagram.com/sanjay_malviya19/">
       <InstagramIcon  className="shadow-[0px_0px_1px_black] bg-white rounded-full p-1" size={30}  cursor={"pointer"}/>
         </Link>
       <Facebook02Icon  className="shadow-[0px_0px_1px_black] bg-white rounded-full p-1" size={30} cursor={"pointer"}/>
-      <TwitterIcon className="shadow-[0px_0px_1px_black] bg-white rounded-full p-1" size={30}  cursor={"pointer"}/>
+      <a className="text-black  border-[1]  border-[#020202] rounded-full cursor-pointer hover:scale-105 duration-300  px-4 py-2  " href="https://wa.me/+918005693569">Contactus</a>
       </div>
+      
     </div>
   )
 }
